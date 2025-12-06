ccd8732d6782afe8ec6b9ce60a94f37b8cb83905
Sujan <sujanramesh91766@gmail.com>
2025-12-07 00:42:31 +0530

    Made the website responsive for mobile screen

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -32,12 +32,13 @@ function MyAppointments() {
               </p>
             </div>
 
-            {/* <div></div> */}
-
+            <div></div>
+            
             <div className="flex flex-col gap-2 justify-end">
               <button className="text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-primary hover:text-white transition-all duration-300">Pay Online</button>
               <button className="text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-red-600 hover:text-white transition-all duration-300">Cancel Appointment</button>
             </div>
+
           </div>
         ))}
       </div>
