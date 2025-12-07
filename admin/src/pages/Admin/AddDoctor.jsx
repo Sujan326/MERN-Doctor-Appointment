bdb6aced508457afb6c39db984e2f9f32e00fd49
Sujan <sujanramesh91766@gmail.com>
2025-12-07 21:50:35 +0530

    Completed Add Doctor Page and fixed errors in API

--- admin/src/pages/Admin/AddDoctor.jsx
+++ admin/src/pages/Admin/AddDoctor.jsx
@@ -1,11 +1,253 @@
-import React from 'react'
+import React from "react";
+import { assets } from "../../assets/assets_admin/assets";
+import { useState } from "react";
+import { useContext } from "react";
+import { AdminContext } from "../../context/AdminContext";
+import { toast } from "react-toastify";
+import axios from "axios";
 
 function AddDoctor() {
+  const [docImage, setDocImage] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [experience, setExperience] = useState(1);
+  const [fees, setFees] = useState("");
+  const [about, setAbout] = useState("");
+  const [speciality, setSpeciality] = useState("General Physician");
+  const [degree, setDegree] = useState("");
+  const [address1, setAddress1] = useState("");
+  const [address2, setAddress2] = useState("");
+
+  const { adminToken, backendUrl } = useContext(AdminContext);
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+
+    try {
+      if (!docImage) {
+        return toast.error("Image Not Selected");
+      }
+
+      const formData = new FormData();
+
+      formData.append("image", docImage);
+      formData.append("name", name);
+      formData.append("email", email);
+      formData.append("password", password);
+      formData.append("experience", experience);
+      formData.append("fees", Number(fees));
+      formData.append("about", about);
+      formData.append("speciality", speciality);
+      formData.append("degree", degree);
+      formData.append(
+        "address",
+        JSON.stringify({ line1: address1, line2: address2 })
+      );
+
+      // console log form data
+      // formData.forEach((value, key) => {
+      //   console.log(`${key} : ${value}`);
+      // });
+
+      const { data } = await axios.post(
+        backendUrl + "/api/admin/add-doctor",
+        formData,
+        { headers: { adminToken } }
+      );
+
+      if (data.success) {
+        toast.success(data.message);
+        setDocImage(false);
+        setName("");
+        setEmail("");
+        setPassword("");
+        setAddress1("");
+        setAddress2("");
+        setDegree("");
+        setAbout("");
+        setFees("");
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+      console.log(error);
+    }
+  };
+
   return (
-    <div>
-      Add Doctor
-    </div>
-  )
+    <form onSubmit={onSubmitHandler} className="m-5 w-full">
+      <p className="mb-3 text-lg font-medium">Add Doctor</p>
+
+      <div className="bg-white px-8 py-8 border rounded w-full max-w-4xl max-h-[80vh] overflow-y-scroll">
+        <div className="flex items-center gap-4 mb-8 text-gray-500">
+          <label htmlFor="doctor-image">
+            <img
+              className="w-16 bg-gray-100 rounded-full cursor-pointer"
+              src={
+                docImage ? URL.createObjectURL(docImage) : assets.upload_area
+              }
+              alt=""
+            />
+          </label>
+          <input
+            type="file"
+            id="doctor-image"
+            hidden
+            onChange={(e) => setDocImage(e.target.files[0])}
+          />
+          <p>
+            Upload Doctor <br /> Picture
+          </p>
+        </div>
+
+        <div className="flex flex-col lg:flex-row items-start gap-10 text-gray-600">
+          <div className="w-full lg:flex-1 flex flex-col gap-4">
+            <div className="flex-1 flex flex-col gap-1">
+              <p>Doctor Name</p>
+              <input
+                className="border rounded px-3 py-2"
+                type="text"
+                placeholder="Name"
+                required
+                onChange={(e) => setName(e.target.value)}
+                value={name}
+              />
+            </div>
+
+            <div className="flex-1 flex flex-col gap-1">
+              <p>Doctor Email</p>
+              <input
+                className="border rounded px-3 py-2"
+                type="email"
+                placeholder="Email"
+                required
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+              />
+            </div>
+
+            <div className="flex-1 flex flex-col gap-1">
+              <p>Doctor Password</p>
+              <input
+                className="border rounded px-3 py-2"
+                type="password"
+                placeholder="Password"
+                required
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+              />
+            </div>
+
+            <div className="flex-1 flex flex-col gap-1">
+              <p>Experience</p>
+              <select
+                onChange={(e) => setExperience(e.target.value)}
+                value={experience}
+                className="border rounded px-3 py-2"
+                name=""
+                id=""
+              >
+                <option value="1 Year">1 Year</option>
+                <option value="2 Year">2 Year</option>
+                <option value="3 Year">3 Year</option>
+                <option value="4 Year">4 Year</option>
+                <option value="5 Year">5 Year</option>
+                <option value="6 Year">6 Year</option>
+                <option value="7 Year">7 Year</option>
+                <option value="8 Year">8 Year</option>
+                <option value="9 Year">9 Year</option>
+                <option value="10 Year">10 Year</option>
+              </select>
+            </div>
+
+            <div className="flex-1 flex flex-col gap-1">
+              <p>Fees</p>
+              <input
+                className="border rounded px-3 py-2"
+                type="number"
+                placeholder="Your Fees"
+                required
+                onChange={(e) => setFees(e.target.value)}
+                value={fees}
+              />
+            </div>
+          </div>
+
+          <div className="w-full lg:flex-1 flex flex-col gap-4">
+            <div className="flex-1 flex flex-col gap-1">
+              <p>Speciality</p>
+              <select
+                onChange={(e) => setSpeciality(e.target.value)}
+                value={speciality}
+                className="border rounded px-3 py-2"
+                name=""
+                id=""
+              >
+                <option value="General Physician">General Physician</option>
+                <option value="Gynecologist">Gynecologist</option>
+                <option value="Dermatologist">Dermatologist</option>
+                <option value="Pediatricians">Pediatricians</option>
+                <option value="Neurologist">Neurologist</option>
+                <option value="Gastroenterologist">Gastroenterologist</option>
+              </select>
+            </div>
+
+            <div className="flex-1 flex flex-col gap-1">
+              <p>Education</p>
+              <input
+                className="border rounded px-3 py-2"
+                type="text"
+                placeholder="Education"
+                required
+                onChange={(e) => setDegree(e.target.value)}
+                value={degree}
+              />
+            </div>
+
+            <div className="flex-1 flex flex-col gap-1">
+              <p>Address</p>
+              <input
+                className="border rounded px-3 py-2"
+                type="text"
+                placeholder="Address Line 1"
+                required
+                onChange={(e) => setAddress1(e.target.value)}
+                value={address1}
+              />
+              <input
+                className="border rounded px-3 py-2"
+                type="text"
+                placeholder="Address Line 2"
+                required
+                onChange={(e) => setAddress2(e.target.value)}
+                value={address2}
+              />
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <p className="mt-4 mb-2">About Doctor</p>
+          <textarea
+            className="w-full px-4 pt-2 border rounded"
+            placeholder="Write about doctor"
+            rows={5}
+            required
+            onChange={(e) => setAbout(e.target.value)}
+            value={about}
+          />
+        </div>
+        <button
+          type="submit"
+          className="bg-primary px-10 py-3 mt-4 text-white rounded-full"
+        >
+          Add Doctor
+        </button>
+      </div>
+    </form>
+  );
 }
 
-export default AddDoctor
+export default AddDoctor;
--- backend/middlewares/authAdmin.js
+++ backend/middlewares/authAdmin.js
@@ -4,7 +4,6 @@ import jwt from "jsonwebtoken";
 const authAdmin = async (req, res, next) => {
   try {
     const adminToken = req.headers.admintoken;
-    console.log("Headers received:", req.headers);
     if (!adminToken) {
       return res.json({
         success: false,
@@ -14,6 +13,12 @@ const authAdmin = async (req, res, next) => {
 
     // decode token
     const tokenDecode = jwt.verify(adminToken, process.env.JWT_SECRET);
+    console.log("Decoded token:", tokenDecode);
+    console.log(
+      "Env values:",
+      process.env.ADMIN_EMAIL,
+      process.env.ADMIN_PASSWORD
+    );
 
     // check the token is valid
     if (
